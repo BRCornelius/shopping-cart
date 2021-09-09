628ec10606ccd6865707eebca6eeee4ee18fea43
Cornelius <bcornelius@iseatz.com>
2021-09-09 10:26:51 -0500

    adds some base style to cart

--- src/ui/components/CartProductsList/CartProductList.jsx
+++ src/ui/components/CartProductsList/CartProductList.jsx
@@ -2,27 +2,32 @@ import React from 'react';
 import { useContext } from 'react';
 import { CartContext } from '../../../context/cart-context';
 
+require('./_CartProductList.css');
+
 export const CartProductList = () => {
   const { cartProducts } = useContext(CartContext);
   let sumTotal = cartProducts.reduce((agg, curr) => agg+=curr.price, 0);
-  return <table>
-      <tr>
-        <th>Product</th>
-        <th>Price</th>
-        <th>Quantity</th>
-        <th></th>
-      </tr>
-      {cartProducts.map(product => <tr key={product.id}>
-        <td>{product.name}</td>
-        <td>1</td>
-        <td>{product.price}</td>
-        <td></td>
-      </tr>)}
-      <tr>
-        <td></td>
-        <td></td>
-        <td>{sumTotal}</td>
-        <td></td>
-      </tr>
-    </table>
+  return <>
+    {cartProducts.length === 0 && <h1>You have no items in your cart</h1>}
+    {cartProducts.length > 0 && <table className="table-container">
+        <tr>
+          <th className="column-first">Product</th>
+          <th>Quantity</th>
+          <th>Price</th>
+          <th>Action</th>
+        </tr>
+        {cartProducts.map(product => <tr key={product.id}>
+          <td className="column-first">{product.name}</td>
+          <td>1</td>
+          <td>{product.price}</td>
+          <td></td>
+        </tr>)}
+        <tr>
+          <td className="column-first"></td>
+          <td>Total</td>
+          <td>{sumTotal}</td>
+          <td></td>
+        </tr>
+      </table>}
+  </>
 }
